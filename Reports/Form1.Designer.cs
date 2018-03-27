2a4c4983f439d3f879e4d11e0e237bbb11161113
HarunYetgindag <37370113+HarunYetgindag@users.noreply.github.com>
2018-03-27 16:23:55 +0300

    Add files via upload

---
+++ Reports/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace Reports
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource3 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource4 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource5 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource6 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.reportViewer2 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.reportViewer3 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.reportViewer4 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.reportViewer5 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.reportViewer6 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.CategoriesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.DataSet = new Reports.DataSet();
+            this.DataTable1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.DataTable2BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.DataTable3BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.DataTable4BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.OrdersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.CategoriesTableAdapter = new Reports.DataSetTableAdapters.CategoriesTableAdapter();
+            this.DataTable1TableAdapter = new Reports.DataSetTableAdapters.DataTable1TableAdapter();
+            this.DataTable2TableAdapter = new Reports.DataSetTableAdapters.DataTable2TableAdapter();
+            this.DataTable3TableAdapter = new Reports.DataSetTableAdapters.DataTable3TableAdapter();
+            this.DataTable4TableAdapter = new Reports.DataSetTableAdapters.DataTable4TableAdapter();
+            this.OrdersTableAdapter = new Reports.DataSetTableAdapters.OrdersTableAdapter();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.tabPage6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CategoriesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable2BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable3BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable4BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1006, 594);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.reportViewer1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(998, 565);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Report 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.reportViewer2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(998, 565);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Report 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.reportViewer3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(998, 565);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Report 3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.reportViewer4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(998, 565);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Report 4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.reportViewer5);
+            this.tabPage5.Location = new System.Drawing.Point(4, 25);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(998, 565);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Report 5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.reportViewer6);
+            this.tabPage6.Location = new System.Drawing.Point(4, 25);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(998, 565);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Report 6";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "Report_1";
+            reportDataSource1.Value = this.CategoriesBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Reports.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(3, 3);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(992, 559);
+            this.reportViewer1.TabIndex = 1;
+            // 
+            // reportViewer2
+            // 
+            this.reportViewer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource2.Name = "Report_2";
+            reportDataSource2.Value = this.DataTable1BindingSource;
+            this.reportViewer2.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportViewer2.LocalReport.ReportEmbeddedResource = "Reports.Report2.rdlc";
+            this.reportViewer2.Location = new System.Drawing.Point(3, 3);
+            this.reportViewer2.Name = "reportViewer2";
+            this.reportViewer2.Size = new System.Drawing.Size(992, 559);
+            this.reportViewer2.TabIndex = 0;
+            // 
+            // reportViewer3
+            // 
+            this.reportViewer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource3.Name = "Report_3";
+            reportDataSource3.Value = this.DataTable2BindingSource;
+            this.reportViewer3.LocalReport.DataSources.Add(reportDataSource3);
+            this.reportViewer3.LocalReport.ReportEmbeddedResource = "Reports.Report3.rdlc";
+            this.reportViewer3.Location = new System.Drawing.Point(3, 3);
+            this.reportViewer3.Name = "reportViewer3";
+            this.reportViewer3.Size = new System.Drawing.Size(992, 559);
+            this.reportViewer3.TabIndex = 0;
+            // 
+            // reportViewer4
+            // 
+            this.reportViewer4.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource4.Name = "Report_4";
+            reportDataSource4.Value = this.DataTable3BindingSource;
+            this.reportViewer4.LocalReport.DataSources.Add(reportDataSource4);
+            this.reportViewer4.LocalReport.ReportEmbeddedResource = "Reports.Report4.rdlc";
+            this.reportViewer4.Location = new System.Drawing.Point(3, 3);
+            this.reportViewer4.Name = "reportViewer4";
+            this.reportViewer4.Size = new System.Drawing.Size(992, 559);
+            this.reportViewer4.TabIndex = 0;
+            // 
+            // reportViewer5
+            // 
+            this.reportViewer5.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource5.Name = "Report_5";
+            reportDataSource5.Value = this.DataTable4BindingSource;
+            this.reportViewer5.LocalReport.DataSources.Add(reportDataSource5);
+            this.reportViewer5.LocalReport.ReportEmbeddedResource = "Reports.Report5.rdlc";
+            this.reportViewer5.Location = new System.Drawing.Point(3, 3);
+            this.reportViewer5.Name = "reportViewer5";
+            this.reportViewer5.Size = new System.Drawing.Size(992, 559);
+            this.reportViewer5.TabIndex = 0;
+            // 
+            // reportViewer6
+            // 
+            this.reportViewer6.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource6.Name = "Report_6";
+            reportDataSource6.Value = this.OrdersBindingSource;
+            this.reportViewer6.LocalReport.DataSources.Add(reportDataSource6);
+            this.reportViewer6.LocalReport.ReportEmbeddedResource = "Reports.Report6.rdlc";
+            this.reportViewer6.Location = new System.Drawing.Point(3, 3);
+            this.reportViewer6.Name = "reportViewer6";
+            this.reportViewer6.Size = new System.Drawing.Size(992, 559);
+            this.reportViewer6.TabIndex = 0;
+            // 
+            // CategoriesBindingSource
+            // 
+            this.CategoriesBindingSource.DataMember = "Categories";
+            this.CategoriesBindingSource.DataSource = this.DataSet;
+            // 
+            // DataSet
+            // 
+            this.DataSet.DataSetName = "DataSet";
+            this.DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // DataTable1BindingSource
+            // 
+            this.DataTable1BindingSource.DataMember = "DataTable1";
+            this.DataTable1BindingSource.DataSource = this.DataSet;
+            // 
+            // DataTable2BindingSource
+            // 
+            this.DataTable2BindingSource.DataMember = "DataTable2";
+            this.DataTable2BindingSource.DataSource = this.DataSet;
+            // 
+            // DataTable3BindingSource
+            // 
+            this.DataTable3BindingSource.DataMember = "DataTable3";
+            this.DataTable3BindingSource.DataSource = this.DataSet;
+            // 
+            // DataTable4BindingSource
+            // 
+            this.DataTable4BindingSource.DataMember = "DataTable4";
+            this.DataTable4BindingSource.DataSource = this.DataSet;
+            // 
+            // OrdersBindingSource
+            // 
+            this.OrdersBindingSource.DataMember = "Orders";
+            this.OrdersBindingSource.DataSource = this.DataSet;
+            // 
+            // CategoriesTableAdapter
+            // 
+            this.CategoriesTableAdapter.ClearBeforeFill = true;
+            // 
+            // DataTable1TableAdapter
+            // 
+            this.DataTable1TableAdapter.ClearBeforeFill = true;
+            // 
+            // DataTable2TableAdapter
+            // 
+            this.DataTable2TableAdapter.ClearBeforeFill = true;
+            // 
+            // DataTable3TableAdapter
+            // 
+            this.DataTable3TableAdapter.ClearBeforeFill = true;
+            // 
+            // DataTable4TableAdapter
+            // 
+            this.DataTable4TableAdapter.ClearBeforeFill = true;
+            // 
+            // OrdersTableAdapter
+            // 
+            this.OrdersTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 595);
+            this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "t";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.CategoriesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable2BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable3BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable4BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource CategoriesBindingSource;
+        private DataSet DataSet;
+        private DataSetTableAdapters.CategoriesTableAdapter CategoriesTableAdapter;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer2;
+        private System.Windows.Forms.BindingSource DataTable1BindingSource;
+        private DataSetTableAdapters.DataTable1TableAdapter DataTable1TableAdapter;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer3;
+        private System.Windows.Forms.BindingSource DataTable2BindingSource;
+        private DataSetTableAdapters.DataTable2TableAdapter DataTable2TableAdapter;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer4;
+        private System.Windows.Forms.BindingSource DataTable3BindingSource;
+        private DataSetTableAdapters.DataTable3TableAdapter DataTable3TableAdapter;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer5;
+        private System.Windows.Forms.BindingSource DataTable4BindingSource;
+        private DataSetTableAdapters.DataTable4TableAdapter DataTable4TableAdapter;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer6;
+        private System.Windows.Forms.BindingSource OrdersBindingSource;
+        private DataSetTableAdapters.OrdersTableAdapter OrdersTableAdapter;
+    }
+}
+
---
+++ Reports/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reports
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'DataSet.Orders' table. You can move, or remove it, as needed.
+            this.OrdersTableAdapter.Fill(this.DataSet.Orders);
+            // TODO: This line of code loads data into the 'DataSet.DataTable4' table. You can move, or remove it, as needed.
+            this.DataTable4TableAdapter.Fill(this.DataSet.DataTable4);
+            // TODO: This line of code loads data into the 'DataSet.DataTable3' table. You can move, or remove it, as needed.
+            this.DataTable3TableAdapter.Fill(this.DataSet.DataTable3);
+            // TODO: This line of code loads data into the 'DataSet.DataTable2' table. You can move, or remove it, as needed.
+            this.DataTable2TableAdapter.Fill(this.DataSet.DataTable2);
+            // TODO: This line of code loads data into the 'DataSet.DataTable1' table. You can move, or remove it, as needed.
+            this.DataTable1TableAdapter.Fill(this.DataSet.DataTable1);
+            // TODO: This line of code loads data into the 'DataSet.Categories' table. You can move, or remove it, as needed.
+            this.CategoriesTableAdapter.Fill(this.DataSet.Categories);
+
+            this.reportViewer1.RefreshReport();
+            this.reportViewer2.RefreshReport();
+            this.reportViewer3.RefreshReport();
+            this.reportViewer4.RefreshReport();
+            this.reportViewer5.RefreshReport();
+            this.reportViewer6.RefreshReport();
+        }
+    }
+}
